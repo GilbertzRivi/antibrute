59724075ebc1078a37e922194943c3a0b88a4c1a
Oktawia <tomek.handerek@gmail.com>
2024-12-12 22:53:23 +0100

    bugfix

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ initial_ban_duration = config["initial_ban_duration"]
 db_path = config["db_path"]
 log_file = config["log_file"]
 
+blocked_ips = []
+
+result = subprocess.run(
+    ["iptables", "-L", "INPUT", "-v", "-n"], capture_output=True, text=True
+)
+rules = result.stdout.splitlines()
+for rule in rules:
+    if "DROP" in rule or "REJECT" in rule:
+        match = re.search(r"\b([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\b", rule)
+        if match:
+            blocked_ips.append(match.group(1))
+
 
 def get_db_connection():
     return sqlite3.connect(db_path, check_same_thread=False)
@@ -54,6 +66,10 @@ CREATE TABLE IF NOT EXISTS ban_count (
 conn.commit()
 
 
+def is_blocked(ip):
+    return ip in blocked_ips
+
+
 def log_message(message):
     print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {message}")
     with open(log_file, "a") as log:
@@ -84,13 +100,15 @@ def ban_ip(ip, duration, ban_number, timeout):
     )
     conn.commit()
     subprocess.run(["iptables", "-A", "INPUT", "-s", ip, "-j", "DROP"])
+    blocked_ips.append(ip)
     log_message(f"IP {ip} banned for {duration} seconds | happened {ban_number} times.")
 
 
-def unban_ip(ip):
+def unban_ip(ip, conn, cursor):
     cursor.execute("DELETE FROM bans WHERE ip = ?", (ip,))
     conn.commit()
     subprocess.run(["iptables", "-D", "INPUT", "-s", ip, "-j", "DROP"])
+    blocked_ips.remove(ip)
     log_message(f"IP {ip} unbanned.")
 
 
@@ -102,23 +120,12 @@ def monitor_bans():
         cursor.execute("SELECT ip FROM bans WHERE ban_end <= ?", (current_time,))
         to_unban = cursor.fetchall()
         for ip in to_unban:
-            unban_ip(ip[0])
+            unban_ip(ip[0], conn, cursor)
         time.sleep(1)
 
 
 def process_request(ip):
     current_time = int(time.time())
-
-    # Usuń stare logi
-    cursor.execute(
-        """
-    DELETE FROM requests WHERE timestamp <= ?
-    """,
-        (current_time - time_window,),
-    )
-    conn.commit()
-
-    # Zapisz nowe żądanie
     cursor.execute(
         """
     INSERT INTO requests (ip, timestamp) VALUES (?, ?)
@@ -130,7 +137,6 @@ def process_request(ip):
     )
     conn.commit()
 
-    # Sprawdź liczbę requestów
     cursor.execute(
         """
     SELECT COUNT(*) FROM requests WHERE ip = ?
@@ -139,7 +145,7 @@ def process_request(ip):
     )
     request_count = cursor.fetchone()[0]
 
-    if request_count > request_limit:
+    if request_count > request_limit and not is_blocked(ip):
         cursor.execute(
             """
         SELECT ban_duration FROM bans WHERE ip = ?
@@ -172,6 +178,21 @@ def process_request(ip):
         ban_ip(ip, new_duration, ban_number, timeout)
 
 
+def delete_old_logs():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    while True:
+        current_time = int(time.time())
+        cursor.execute(
+            """
+        DELETE FROM requests WHERE timestamp <= ?
+        """,
+            (current_time - 24 * 60 * 60,),
+        )
+        conn.commit()
+        time.sleep(60)
+
+
 def monitor_traffic():
     log_message("Started monitoring traffic.")
 
@@ -191,8 +212,8 @@ def monitor_traffic():
                     match = re.search(r"SRC=([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)", line)
                     if match:
                         ip = match.group(1)
-                        process_request(ip)
                         log_message(f"processing {ip}")
+                        process_request(ip)
         except Exception as e:
             log_message(f"Error in monitor_traffic: {str(e)}")
         finally:
@@ -239,11 +260,8 @@ if __name__ == "__main__":
         ban_thread.start()
         timeout_thread = Thread(target=monitor_timeout, daemon=True)
         timeout_thread.start()
-        monitor_thread = Thread(target=monitor_traffic, daemon=True)
-        monitor_thread.start()
 
-        for thread in [ban_thread, timeout_thread, monitor_thread]:
-            thread.join()
+        monitor_traffic()
 
     except KeyboardInterrupt:
         log_message("Script terminated.")
